b278ef7a86f8f7b9469c8f2322437a8cce920178
tegzwn <tegzwn@outlook.com>
2025-12-07 15:29:01 +0000

    airoha: add support for Nokia-Bell XG-040G-MD This commit adds support for Nokia-Bell XG-040G-MD.
    Device specification
    --------------------
    SoC Type:  Airoha AN7581
    RAM:    JHICC CA4S4G16V (512MiB)
    Flash:    Fudan Micro FM25G02BI3/Sky High S35ML02G300 (256MiB)
    Ethernet:  3x gigabit via AN7581 (eth0: LAN2, LAN3, LAN4)
                    1x 2.5gigabit via Airoha EH8811H (eth1: LAN1)
    
    Signed-off-by: tegzwn <tegzwn@outlook.com>

--- target/linux/airoha/image/an7581.mk
+++ target/linux/airoha/image/an7581.mk
@@ -60,3 +60,25 @@ define Device/gemtek_w1700k
   SOC := an7581
 endef
 TARGET_DEVICES += gemtek_w1700k
+
+define Device/bell_xg-040g-md
+  $(call Device/FitImageLzma)
+  DEVICE_VENDOR := Nokia Bell
+  DEVICE_MODEL := Nokia Bell XG-040G-MD
+  DEVICE_DTS_CONFIG := config@1
+  KERNEL_LOADADDR := 0x80088000
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_SIZE := 10240k
+  IMAGE_SIZE := 261120k
+  KERNEL_IN_UBI := 1
+  UBINIZE_OPTS := -m 2048 -p 128KiB -s 2048
+  DEVICE_PACKAGES := kmod-phy-airoha-en8811h \
+         kmod-i2c-an7581 kmod-gpio-button-hotplug
+  IMAGES += factory.bin sysupgrade.bin
+  IMAGE/factory.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  SOC := an7581
+endef
+TARGET_DEVICES += bell_xg-040g-md
